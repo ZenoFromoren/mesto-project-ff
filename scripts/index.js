8c0031e2657a6d1102b38df16dd8efacea6a5a6d
Vladislav Baranov <briliant645@gmail.com>
2023-11-28 13:28:58 +0500

    Первая версия

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,25 @@
 // @todo: Темплейт карточки
+const places = document.querySelector('.places');
+const placesList = places.querySelector('.places__list');
+
+const deleteCard = evt => evt.target.parentElement.remove();
+
+const addCard = (cardTitle, cardImage, deleteCard) => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  cardElement.querySelector('.card__title').textContent = cardTitle;
+  cardElement.querySelector('.card__image').src = cardImage;
+
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', deleteCard);
+
+  return cardElement;
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+  placesList.append(addCard(initialCards[i].name, initialCards[i].link, deleteCard));
+}
 
 // @todo: DOM узлы
 
